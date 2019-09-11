a7b92db7c3b930094b544188f8a367180d537b45
Mahashraya Sundararaman <mahashraya@Mahashrayas-MacBook-Pro.local>
2019-09-10 20:28:47 -0400

    more stuff

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import os
 load_file = os.path.join("budget_data.csv")
 output_file = os.path.join("budget_analysis.txt")
 
-# Create lists and counters for each of the parameters in your budget analysis
+# Create lists and counters for each of the parameters in budget analysis
 total_months = 0
 month = []
-net_d_pnl = []
+net_change_list = []
 greatest_inc_pnl = ["", 0]
 greatest_dec_pnl = ["", 9999999999999999999]
-total_net_d = 0
+net_pnl = 0
 
 
 # Read csv and convert to list of dictionaries
@@ -22,10 +22,10 @@ with open(load_file) as budget_data:
     # Read header row
     header = next(reader)
 
-    # Extract first row
+    # Extract first row to avoid double-counting 
     first_row = next(reader)
     total_months = total_months + 1
-    net_pnl = net_d_pnl + int(first_row[1])
+    net_pnl = net_pnl + int(first_row[1])
     prev_net = int(first_row[1])
 
     for row in reader:
@@ -33,5 +33,8 @@ with open(load_file) as budget_data:
         # Track the total number of months and total net Profit and Losses
         total_months = total_months + 1
         net_pnl = net_pnl + int(row[1])
+
+        # Track the net change in Profit and Losses
+        net_change = 
 
 
